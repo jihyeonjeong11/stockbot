dec9060904c2f9e02e3ad2b9d770d21a3bf73398
jihyeonjeong11 <wjdwlgus11@gmail.com>
2023-02-19 19:48:04 +0900

    2/2

--- KIS_Common.py
+++ KIS_Common.py
@@ -1,4 +1,8 @@
 import yaml
+import pprint
+
+import json
+import requests
 
 
 stock_info = None
@@ -38,7 +42,7 @@ def GetAppSecret(dist = "REAL"):
     return stock_info[key]
 
 
-#계좌 정보(계좌번호)를 리턴!
+#계좌 정보를 리턴!
 def GetAccountNo(dist = "REAL"):
     global stock_info
     
@@ -52,7 +56,7 @@ def GetAccountNo(dist = "REAL"):
     return stock_info[key]
 
 
-#계좌 정보(삼품코드)를 리턴!
+#계좌 정보를 리턴!
 def GetPrdtNo(dist = "REAL"):
     global stock_info
     
@@ -83,6 +87,76 @@ def GetUrlBase(dist = "REAL"):
 
 
 
+#토큰 저장할 경로
+def GetTokenPath(dist = "REAL"):
+    global stock_info
+    
+    key = ""
+    
+    if dist == "REAL":
+        key = "REAL_TOKEN_PATH"
+    elif dist == "VIRTUAL":
+        key = "VIRTUAL_TOKEN_PATH"
         
+    return stock_info[key]
+
+
+        
+
+#토큰 값을 리퀘스트 해서 실제로 만들어서 파일에 저장하는 함수!! 첫번째 파라미터: "REAL" 실계좌, "VIRTUAL" 모의계좌
+def MakeToken(dist = "REAL"):
+
+
+    headers = {"content-type":"application/json"}
+    body = {
+        "grant_type":"client_credentials",
+        "appkey":GetAppKey(dist), 
+        "appsecret":GetAppSecret(dist)
+        }
+
+    PATH = "oauth2/tokenP"
+    URL = f"{GetUrlBase(dist)}/{PATH}"
+    res = requests.post(URL, headers=headers, data=json.dumps(body))
     
-    
+
+    if res.status_code == 200:
+        my_token = res.json()["access_token"]
+
+        #빈 딕셔너리를 선언합니다!
+        dataDict = dict()
+
+        #해당 토큰을 파일로 저장해 둡니다!
+        dataDict["authorization"] = my_token
+        with open(GetTokenPath(dist), 'w') as outfile:
+            json.dump(dataDict, outfile)   
+
+        print("TOKEN : ", my_token)
+
+        return my_token
+
+    else:
+        print('Get Authentification token fail!')  
+        return "FAIL"
+
+
+#파일에 저장된 토큰값을 읽는 함수.. 만약 파일이 없다면 MakeToken 함수를 호출한다!
+def GetToken(dist = "REAL"):
+        
+    #빈 딕셔너리를 선언합니다!
+    dataDict = dict()
+
+    try:
+
+        #이 부분이 파일을 읽어서 딕셔너리에 넣어주는 로직입니다. 
+        with open(GetTokenPath(dist), 'r') as json_file:
+            dataDict = json.load(json_file)
+
+
+        return dataDict['authorization']
+
+    except Exception as e:
+        print("Exception by First")
+
+        #처음에는 파일이 존재하지 않을테니깐 바로 토큰 값을 구해서 리턴!
+        return MakeToken(dist)
+
--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 import KIS_Common as common
 
 
-print("REAL_APP_KEY:" , common.GetAppKey("REAL"))
-print("REAL_APP_SECRET:" , common.GetAppSecret("REAL"))
-print("REAL_CANO" , common.GetAccountNo("REAL"))
-print("REAL_ACNT_PRDT_CD" , common.GetPrdtNo("REAL"))
-print("REAL_URL" , common.GetUrlBase("REAL"))
+print("REAL TOKEN" , common.GetToken("REAL"))
 
 print("----------------------------------------------------")
 
-print("VIRTUAL_APP_KEY:" , common.GetAppKey("VIRTUAL"))
-print("VIRTUAL_APP_SECRET:" , common.GetAppSecret("VIRTUAL"))
-print("VIRTUAL_CANO" , common.GetAccountNo("VIRTUAL"))
-print("VIRTUAL_ACNT_PRDT_CD" , common.GetPrdtNo("VIRTUAL"))
-print("VIRTUAL_URL" , common.GetUrlBase("VIRTUAL"))
+#print("VIRTUAL TOKEN:" , common.GetToken("VIRTUAL"))
+
+
+
+
