1feba55e4f11a58871c949d99787e28df3de1fc6
jihyeonjeong11 <wjdwlgus11@gmail.com>
2023-02-19 20:22:14 +0900

    2/3

--- KIS_Common.py
+++ KIS_Common.py
@@ -1,9 +1,10 @@
 import yaml
-import pprint
 
 import json
 import requests
 
+from datetime import datetime, timedelta
+from pytz import timezone
 
 stock_info = None
 
@@ -12,6 +13,23 @@ with open('./var/autobot/myStockInfo.yaml', encoding='UTF-8') as f:
     stock_info = yaml.load(f, Loader=yaml.FullLoader)
     
     
+
+############################################################################################################################################################
+NOW_DIST = ""
+
+#계좌 전환 함수! REAL 실계좌 VIRTUAL 모의계좌
+def SetChangeMode(dist = "REAL"):
+    global NOW_DIST 
+    NOW_DIST = dist
+
+
+#현재 선택된 계좌정보를 리턴!
+def GetNowDist():
+    global NOW_DIST 
+    return NOW_DIST
+
+############################################################################################################################################################
+
 #앱 키를 반환하는 함수
 def GetAppKey(dist = "REAL"):
     
@@ -114,12 +132,11 @@ def MakeToken(dist = "REAL"):
         "appsecret":GetAppSecret(dist)
         }
 
-    
-
     PATH = "oauth2/tokenP"
     URL = f"{GetUrlBase(dist)}/{PATH}"
     res = requests.post(URL, headers=headers, data=json.dumps(body))
     
+
     if res.status_code == 200:
         my_token = res.json()["access_token"]
 
@@ -160,4 +177,61 @@ def GetToken(dist = "REAL"):
 
         #처음에는 파일이 존재하지 않을테니깐 바로 토큰 값을 구해서 리턴!
         return MakeToken(dist)
+
+
+############################################################################################################################################################
+#해시키를 리턴한다!
+def GetHashKey(datas):
+
+    PATH = "uapi/hashkey"
+    URL = f"{GetUrlBase(NOW_DIST)}/{PATH}"
+
+    headers = {
+    'content-Type' : 'application/json',
+    'appKey' : GetAppKey(NOW_DIST),
+    'appSecret' : GetAppSecret(NOW_DIST),
+    }
+
+    res = requests.post(URL, headers=headers, data=json.dumps(datas))
+
+    if res.status_code == 200 :
+        return res.json()["HASH"]
+    else:
+        print("Error Code : " + str(res.status_code) + " | " + res.text)
+        return None
+
+############################################################################################################################################################
+
+
+############################################################################################################################################################
+#한국인지 미국인지 구분해 현재 날짜정보를 리턴해 줍니다!
+def GetNowDateStr(area = "KR", type= "NONE" ):
+    timezone_info = timezone('Asia/Seoul')
+    if area == "US":
+        timezone_info = timezone('America/New_York')
+
+    now = datetime.now(timezone_info)
+    if type.upper() == "NONE":
+        return now.strftime("%Y%m%d")
+    else:
+        return now.strftime("%Y-%m-%d")
+
+#현재날짜에서 이전/이후 날짜를 구해서 리턴! (미래의 날짜를 구할 일은 없겠지만..)
+def GetFromNowDateStr(area = "KR", type= "NONE" , days=100):
+    timezone_info = timezone('Asia/Seoul')
+    if area == "US":
+        timezone_info = timezone('America/New_York')
+
+    now = datetime.now(timezone_info)
+
+    if days < 0:
+        next = now - timedelta(days=abs(days))
+    else:
+        next = now + timedelta(days=days)
+
+    if type.upper() == "NONE":
+        return next.strftime("%Y%m%d")
+    else:
+        return next.strftime("%Y-%m-%d")
+############################################################################################################################################################
 
