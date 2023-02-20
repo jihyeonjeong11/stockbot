85bf34c87f0665f94f034d186ade20b3732f5a88
jihyeonjeong11 <wjdwlgus11@gmail.com>
2023-02-20 21:40:29 +0900

    2-5

--- test_kr.py
+++ test_kr.py
@@ -51,54 +51,54 @@ print("                                     ")
 
 
 #삼성전자 2주 시장가 매수
-pprint.pprint(KisKR.MakeBuyMarketOrder(stock_code,2))
+pprint.pprint(KisKR.MakeBuyMarketOrder(stock_code,1))
 
 print("                                     ")
 print("------------------------------------")
 print("                                     ")
 
-#영상엔 안 나와 있지만
-#모의계좌의 경우 시장가 매수의 경우에도 딜레이가 걸리는 경우가 있습니다.
-#즉 지연체결 문제가 있다는 점을 알고 계세요~!! 
-#3초 정도 쉬어주지만 3초 후에 주문 체결이 완료 된다는 보장은 없습니다!
-time.sleep(3.0)
+# #영상엔 안 나와 있지만
+# #모의계좌의 경우 시장가 매수의 경우에도 딜레이가 걸리는 경우가 있습니다.
+# #즉 지연체결 문제가 있다는 점을 알고 계세요~!! 
+# #3초 정도 쉬어주지만 3초 후에 주문 체결이 완료 된다는 보장은 없습니다!
+# time.sleep(3.0)
 
 
-#삼성전자 1주 시장가 매도
-pprint.pprint(KisKR.MakeSellMarketOrder(stock_code,1))
+# #삼성전자 1주 시장가 매도
+# pprint.pprint(KisKR.MakeSellMarketOrder(stock_code,1))
 
-print("                                     ")
-print("------------------------------------")
-print("                                     ")
-
-
-buy_price = current_price * 0.9
-
-#삼성전자 1주 지정가 매수
-pprint.pprint(KisKR.MakeBuyLimitOrder(stock_code,1,buy_price))
-
-print("                                     ")
-print("------------------------------------")
-print("                                     ")
+# print("                                     ")
+# print("------------------------------------")
+# print("                                     ")
 
 
+# buy_price = current_price * 0.9
 
+# #삼성전자 1주 지정가 매수
+# pprint.pprint(KisKR.MakeBuyLimitOrder(stock_code,1,buy_price))
 
-sell_price = current_price * 1.1
-
-#삼성전자 1주 지정가 매도 
-pprint.pprint(KisKR.MakeSellLimitOrder(stock_code,1,sell_price))
-
-print("                                     ")
-print("------------------------------------")
-print("                                     ")
+# print("                                     ")
+# print("------------------------------------")
+# print("                                     ")
 
 
 
 
-#전체 주문리스트에서 현재 오픈된 주문을 가져온다
-pprint.pprint(KisKR.GetOrderList("","ALL","OPEN"))
+# sell_price = current_price * 1.1
 
-print("                                     ")
-print("------------------------------------")
-print("                                     ")
+# #삼성전자 1주 지정가 매도 
+# pprint.pprint(KisKR.MakeSellLimitOrder(stock_code,1,sell_price))
+
+# print("                                     ")
+# print("------------------------------------")
+# print("                                     ")
+
+
+
+
+# #전체 주문리스트에서 현재 오픈된 주문을 가져온다
+# pprint.pprint(KisKR.GetOrderList("","ALL","OPEN"))
+
+# print("                                     ")
+# print("------------------------------------")
+# print("                                     ")
